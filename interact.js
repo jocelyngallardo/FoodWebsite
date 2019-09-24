004351635b25c21323a9d905dac94c2125619dce
coder123n <naomisellis@gmail.com>
2019-09-24 09:08:37 -0700

    interat

--- interact.js
+++ interact.js
@@ -2,3 +2,13 @@
 	toggle with side dishes
 	toggle with seasonal drinks
  */
+ $(document).ready(function(){
+	$("h1").click(function(){
+		$("p").toggle();
+		$("img").toggle;
+	});
+	$("#one").click(function(){
+		$(".hide").toggle();
+		$("img").show();
+	});
+});
