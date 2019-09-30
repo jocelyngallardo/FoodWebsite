546487d36636ff7c4c580a3f6e4125da9988d843
Jocelyn Gallardo <Jocelyn@My-MacBook-Pro.local>
2019-09-29 21:32:27 -0700

    add fadeIn()

--- interact.js
+++ interact.js
@@ -3,6 +3,10 @@
 	toggle with seasonal drinks
  */
  $(document).ready(function(){
+   $("h2").click(function(){
+  $("#appleFloats").fadeIn();
+  $("#floatRecipe").fadeIn();
+});
 	/*$("#click").click(function(){;
 		$("#drinkText").toggle();
 		$("#drinkRecipe").toggle();
@@ -10,7 +14,7 @@
 	});
 	$(".appear").attr();
 	});*/
-	$("#click").click(function(){
+	/*$("#click").click(function(){
 		$('#bk').attr('src','Beans.jpg');
-	});	
-}); 
+	});	*/
+});
