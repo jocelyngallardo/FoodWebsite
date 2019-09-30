16900c1ed18d95d005feb9c3cddd133f8946af99
jocelyngallardo <jocelynggallardo@gmail.com>
2019-09-29 21:50:03 -0700

    remove some comments from js

--- interact.js
+++ interact.js
@@ -1,19 +1,10 @@
-/*seasonal drink website link
-	toggle with side dishes
-	toggle with seasonal drinks
- */
  $(document).ready(function(){
    $("h2").click(function(){
    $(".floatRecipe").fadeToggle();
    $("#appleFloats").fadeToggle();
 });
-	/*$("#click").click(function(){;
-		$("#drinkText").toggle();
-		$("#drinkRecipe").toggle();
-		$("#appleFloats").toggle()
-	});
 	$(".appear").attr();
-	});*/
+	});
 	/*$("#click").click(function(){
 		$('#bk').attr('src','Beans.jpg');
 	});	*/
